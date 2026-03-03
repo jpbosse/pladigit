b1ac23beaafe67693c991579b501d73fe58fd74e
Jean-Pierre Bossé <jpbosse1@gmail.com>
2026-03-03 16:17:08 +0100

    style: fix pint binary_operator_spaces dans lang/fr

--- lang/fr/Passwords.php
+++ lang/fr/Passwords.php
@@ -8,10 +8,10 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'reset'     => 'Votre mot de passe a été réinitialisé.',
-    'sent'      => 'Nous vous avons envoyé un e-mail contenant le lien de réinitialisation.',
+    'reset' => 'Votre mot de passe a été réinitialisé.',
+    'sent' => 'Nous vous avons envoyé un e-mail contenant le lien de réinitialisation.',
     'throttled' => 'Veuillez patienter avant de réessayer.',
-    'token'     => 'Ce lien de réinitialisation est invalide ou expiré.',
-    'user'      => 'Aucun utilisateur n\'a été trouvé avec cette adresse e-mail.',
+    'token' => 'Ce lien de réinitialisation est invalide ou expiré.',
+    'user' => 'Aucun utilisateur n\'a été trouvé avec cette adresse e-mail.',
 
 ];
